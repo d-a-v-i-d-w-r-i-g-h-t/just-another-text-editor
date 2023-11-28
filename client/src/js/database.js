224cb8ff1209923d684645c1a832e72235db6fe1
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-27 22:32:48 -0800

    Add putDb function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,16 +13,27 @@ const initdb = async () =>
   });
 
   
-// instead of CRUD, just add RU: Read (GET), Update (PUT)
-
 // export a function to Update (PUT) the database
-export const putDb = async (content) => console.error('putDb not implemented');
-  // TODO: Add logic to a method that accepts some content and adds it to the database
-    // create an openDB - Database name, version.
-    // create a transaction - database name, readwrite, 
-    // create a store - store name, 
-    // put the store - content: {id, info }
-    // await the request
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  // create a connection with the database and specify the version
+  const jateDb = await openDB('jate', 1);
+
+  // create a new transaction, specifying the database and data privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // open the desired object store in the database (it has the same name as the database)
+  const store = tx.objectStore('jate');
+
+  // update the data record from the object store (there is only one record)
+  const request = store.put({ id: id, jate: content });
+
+  // get confirmation of the request
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+  
 
 // export a function to Read (GET) the record from the database (there is only one)
 export const getDb = async (id) => {
