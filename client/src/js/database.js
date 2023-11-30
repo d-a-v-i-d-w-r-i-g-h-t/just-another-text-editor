14bec2337e3b6da196a0dedac5fa7b844050c5b1
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-29 22:13:40 -0800

    Fix database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,16 +28,17 @@ export const putDb = async (content) => {
 
   // update the data record from the object store (there is only one record)
   // const request = store.put({ id: id, jate: content });
-  const request = store.put({ jate: content });
+  const request = store.put({ id: 1, content });
 
   // get confirmation of the request
   const result = await request;
   console.log('Data saved to the database', result);
+  return result;
 };
   
 
 // export a function to Read (GET) the record from the database (there is only one)
-export const getDb = async (id) => {
+export const getDb = async () => {
   console.log('GET from the database');
 
   // create a connection with the database and specify the version
@@ -50,35 +51,13 @@ export const getDb = async (id) => {
   const store = tx.objectStore('jate');
 
   // get the data record from the object store (there is only one record)
-  const request = store.get(id);
+  const request = store.get(1);
 
   // get confirmation of the request
   const result = await request;
-  console.log('data has been retrieved', result);
-  return result;
+  console.log('Data has been retrieved from the database', result);
+  return result.content;
 };
-
-// // export a function to Read (GET) the record from the database (there is only one)
-// export const getDb = async () => {
-//   console.log('GET from the database');
-
-//   // create a connection with the database and specify the version
-//   const jateDb = await openDB('jate', 1);
-
-//   // create a new transaction, specifying the database and data privileges
-//   const tx = jateDb.transaction('jate', 'readonly');
-
-//   // open the desired object store in the database (it has the same name as the database)
-//   const store = tx.objectStore('jate');
-
-//   // get the data record from the object store (there is only one record)
-//   const request = store.getAll();
-
-//   // get confirmation of the request
-//   const result = await request;
-//   console.log('data has been retrieved', result);
-//   return result;
-// };
 
 // start the database
 initdb();
