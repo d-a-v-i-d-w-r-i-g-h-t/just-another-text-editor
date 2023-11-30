130d7a1edf1bc4fe48d23eb4cbdde14c43cd8379
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-11-29 22:08:16 -0800

    Add .npmrc

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,7 +27,8 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // update the data record from the object store (there is only one record)
-  const request = store.put({ id: id, jate: content });
+  // const request = store.put({ id: id, jate: content });
+  const request = store.put({ jate: content });
 
   // get confirmation of the request
   const result = await request;
@@ -56,6 +57,28 @@ export const getDb = async (id) => {
   console.log('data has been retrieved', result);
   return result;
 };
+
+// // export a function to Read (GET) the record from the database (there is only one)
+// export const getDb = async () => {
+//   console.log('GET from the database');
+
+//   // create a connection with the database and specify the version
+//   const jateDb = await openDB('jate', 1);
+
+//   // create a new transaction, specifying the database and data privileges
+//   const tx = jateDb.transaction('jate', 'readonly');
+
+//   // open the desired object store in the database (it has the same name as the database)
+//   const store = tx.objectStore('jate');
+
+//   // get the data record from the object store (there is only one record)
+//   const request = store.getAll();
+
+//   // get confirmation of the request
+//   const result = await request;
+//   console.log('data has been retrieved', result);
+//   return result;
+// };
 
 // start the database
 initdb();
